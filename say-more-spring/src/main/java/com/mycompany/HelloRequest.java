19852e833d7df6effe969221c1719d558c5777fe
Brian Fitzpatrick <bfitzpat@redhat.com>
2017-11-15 12:12:37 -0700

    Adding projects in process

---
+++ say-more-spring/src/main/java/com/mycompany/HelloRequest.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+public class HelloRequest {
+
+	public String name;
+	
+	public HelloRequest() {
+		// empty
+	}
+	
+	public HelloRequest(String name) {
+		this.name = name;
+	}
+}
