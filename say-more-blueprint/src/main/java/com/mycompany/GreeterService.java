529e8b48a87501279a61adf9a04bbca9b709c0ef
Brian Fitzpatrick <bfitzpat@redhat.com>
2017-11-27 13:41:25 -0700

    Fixes suggested by Freeman for say-more-blueprint

--- say-more-blueprint/src/main/java/com/mycompany/GreeterService.java
+++ say-more-blueprint/src/main/java/com/mycompany/GreeterService.java
@@ -3,6 +3,11 @@ package com.mycompany;
 public class GreeterService {
 
 	public HelloResponse createHello(HelloRequest request) throws Exception {
-		return new HelloResponse("SUCCESS", new String[] { "warn1", "warn2" });
+		if (request == null) {
+			return new HelloResponse("FAILURE", new String[] {"HelloRequest was null"});
+		} else if (request.name == null || request.name.isEmpty()) {
+			return new HelloResponse("FAILURE", new String[] {"HelloRequest.name was empty"});
+		}
+		return new HelloResponse("SUCCESS:" + request.name);
 	}
 }
--- say-more-blueprint/src/main/java/com/mycompany/HelloResponse.java
+++ say-more-blueprint/src/main/java/com/mycompany/HelloResponse.java
@@ -8,6 +8,11 @@ public class HelloResponse {
 		// empty
 	}
 	
+	public HelloResponse(String response) {
+		this.response = response;
+		this.errors = null;
+	}
+
 	public HelloResponse(String response, String[] errors) {
 		this.response = response;
 		this.errors = errors;
